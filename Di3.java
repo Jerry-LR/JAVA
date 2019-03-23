cca8394b5f1c099a3c85c8c64b2cbb79c9dd7c68
Jerry-LR <48666640+Jerry-LR@users.noreply.github.com>
2019-03-23 20:45:17 +0800

    Add files via upload

---
+++ Di3.java
@@ -0,0 +1,19 @@
+package com.jerry.leikulianxi;
+
+public class Di3 {
+	public static void main(String[] args) {
+		String a="Hello World! dsadsa dsada dsa dsa";
+		String[] b=a.split(" ", 0);
+		int c=b.length;
+		for(String k:b) {
+			System.out.println(k);
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
